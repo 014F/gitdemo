48e43913317e2a25269b0a9be35d8bc9ca49ffb6
014F <ofalcio@gmail.com>
2021-11-01 18:57:25 +0100

    Add a new project to Git

---
+++ cmake-build-debug/CMakeFiles/gitdemo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitdemo.dir/main.cpp.obj"
+  "gitdemo.exe"
+  "gitdemo.exe.manifest"
+  "gitdemo.pdb"
+  "libgitdemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std; //po co pisać to std?
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Zartowalem, Hello friend!" << endl;
+    return 0;
+}
