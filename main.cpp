69e2a7bf6da104131e568589c534f9b62c3888d6
014F <ofalcio@gmail.com>
2021-11-01 19:22:45 +0100

    Hello friend

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 using namespace std; //po co pisać to std?
 int main() {
-    cout << "Some changes from the master branch" << endl;
-    cout << "Zartowalem, Hello friend!" << endl;
-    cout << "Zartowalem, Hello friend!" << endl;
-
-    cout << "Czy to dziala?" << endl;
-
+    cout << "Hello friend" << endl;
     return 0;
 }
